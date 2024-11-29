b17d82e4f7e50388c384703dcf395883325a9b2b
o9zs <arsenyojike@gmail.com>
2024-11-29 16:26:40 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,65 @@
+import asyncio
+import random
+
+from telethon import TelegramClient, utils
+from telethon.events import NewMessage
+
+import config
+
+client = TelegramClient("telethon", config.API_ID, config.API_HASH, system_version="5.5.5")
+
+if config.auto_respond == True:
+	cached_users = []
+
+	@client.on(NewMessage(incoming=True, func=lambda e: e.is_private))
+	async def respond(event: NewMessage.Event):
+		sender = await event.get_sender()
+
+		if sender in cached_users: return
+		cached_users.append(sender)
+
+		await asyncio.sleep(config.read_after)
+		await client.send_read_acknowledge(sender)
+
+		async with client.action(sender, 'typing'):
+			await asyncio.sleep(config.respond_after)
+			await client.send_message(sender, random.choice(config.responses), parse_mode="markdown")
+
+		print("Responded to user")
+			
+async def send_to_chats():
+	async for dialog in client.iter_dialogs():
+		if dialog.is_group:
+			dialog_id, _ = utils.resolve_id(dialog.id)
+
+			if dialog_id in config.excluded_chats: return
+
+			try:
+				if config.forward_from_channel == True:
+					if dialog_id in config.per_chat_ids:
+						message_ids = config.per_chat_ids[dialog_id]
+					else:
+						message_ids = config.message_ids
+
+					await client.forward_messages(dialog, random.choice(message_ids), 2415085452)
+				else:
+					await client.send_message(dialog, random.choice(config.messages))
+
+				print(f"{dialog.name} has been a success")
+			except Exception as exception:
+				print(f"{dialog.name} says \"{exception}\"")
+
+				continue
+
+	print("\n")
+
+async def mail():
+	if config.mail == True:
+		while True:
+			await send_to_chats()
+			
+			await asyncio.sleep(config.interval)
+
+with client:
+    client.loop.create_task(mail())
+    client.loop.run_forever()
---
+++ config.py
@@ -0,0 +1,31 @@
+API_ID = 27806506
+API_HASH = "c3b39a0ffbcc698afb63319c95e97b61"
+
+responses = [
+	"**Весь ворк тут 🤗**\nА также конкурсы, призы и многое другое ↙\n\n[https://t.me/t0rtuga_chat](https://t.me/+3BvFRB3JSKViMDY0)",
+	"**Весь ворк тут 💰**\nА также конкурсы, призы и многое другое ↙\n\n[https://t.me/t0rtuga_chat](https://t.me/+3BvFRB3JSKViMDY0)",
+	"**Весь ворк тут 👾**\nА также конкурсы, призы и многое другое ↙\n\n[https://t.me/t0rtuga_chat](https://t.me/+3BvFRB3JSKViMDY0)",
+	"**Весь ворк тут 💎**\nА также конкурсы, призы и многое другое ↙\n\n[https://t.me/t0rtuga_chat](https://t.me/+3BvFRB3JSKViMDY0)"
+]
+
+messages = [
+	
+]
+
+# Toggles
+mail = True
+auto_respond = True
+forward_from_channel = True
+
+# IDs
+excluded_chats = [2295959373, 2038629185]
+channel_id = 2415085452
+message_ids = [22, 23, 25, 31, 31, 31]
+per_chat_ids = {
+	1566598220: [35, 36, 38, 39, 39, 39]
+}
+
+# Intervals
+interval = 300
+read_after = 3
+respond_after = 3
