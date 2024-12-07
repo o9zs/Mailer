c99c1c431a51bd98acd2e0a6915a53927e889344
o9zs <arsenyojike@gmail.com>
2024-12-07 13:57:06 +0300

    Added tuples and changed responses

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ API_HASH = "c3b39a0ffbcc698afb63319c95e97b61"
 invite_link = "https://t.me/+F_nRWmsKzPwxZjk0"
 
 responses = [
-	f"**Весь ворк тут 🤗**\nА также конкурсы, призы и многое другое ↙\n\n[https://t.me/t0rtuga_chat]({invite_link})",
-	f"**Весь ворк тут 💰**\nА также конкурсы, призы и многое другое ↙\n\n[https://t.me/t0rtuga_chat]({invite_link})",
-	f"**Весь ворк тут 👾**\nА также конкурсы, призы и многое другое ↙\n\n[https://t.me/t0rtuga_chat]({invite_link})",
-	f"**Весь ворк тут 💎**\nА также конкурсы, призы и многое другое ↙\n\n[https://t.me/t0rtuga_chat]({invite_link})"
+	f"**Весь ворк тут 🤗**\nА также конкурсы, призы и многое другое ↙\n\n[https://t.me/t0rtuga_chat]({invite_link})\n\nА на данный момент идет конкурс [здесь](https://t.me/CryptoBot/app?startapp=giveaway-GHlEp9xBrzdEv-GMVsiKRx3rtshz)",
+	f"**Весь ворк тут 💰**\nА также конкурсы, призы и многое другое ↙\n\n[https://t.me/t0rtuga_chat]({invite_link})\n\nА на данный момент идет конкурс [здесь](https://t.me/CryptoBot/app?startapp=giveaway-GHlEp9xBrzdEv-GMVsiKRx3rtshz)",
+	f"**Весь ворк тут 👾**\nА также конкурсы, призы и многое другое ↙\n\n[https://t.me/t0rtuga_chat]({invite_link})\n\nА на данный момент идет конкурс [здесь](https://t.me/CryptoBot/app?startapp=giveaway-GHlEp9xBrzdEv-GMVsiKRx3rtshz)",
+	f"**Весь ворк тут 💎**\nА также конкурсы, призы и многое другое ↙\n\n[https://t.me/t0rtuga_chat]({invite_link})\n\nА на данный момент идет конкурс [здесь](https://t.me/CryptoBot/app?startapp=giveaway-GHlEp9xBrzdEv-GMVsiKRx3rtshz)"
 ]
 
 messages = [
@@ -28,6 +28,6 @@ per_chat_ids = {
 excluded_chats = [2295959373, 2038629185]
 hide_forward_chats = [2091490622, 2315505220]
 
-interval = 300
-read_after = 3
-respond_after = 3
+interval = (300, 600)
+read_after = (2, 5)
+respond_after = (2, 5)
