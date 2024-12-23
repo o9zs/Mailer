582493078d8b7f4a5bd4810d20ff70333ed050a6
opzs <99386990+o9zs@users.noreply.github.com>
2024-12-23 21:22:36 +0300

    New link

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 API_ID = 27806506
 API_HASH = "c3b39a0ffbcc698afb63319c95e97b61"
 
-invite_link = "https://t.me/+CVDHganalCNlNWNk"
+invite_link = "https://t.me/+2DQ6NK5VihBhZWFk"
 
 responses = []
 
